a189bf7e737bd603338f0867b4cb386a83159e56
ESDUNCAN <d.eric@rogers.com>
2022-04-16 22:57:47 +0000

    updated js in terminal

---
+++ conditionals.js
@@ -0,0 +1,28 @@
+age = 15
+whichschool = if(age <13){
+  whichSchool = "Elementary School"
+} else if (age > 13 && age <18 ){
+  whichSchool = "Secondary School"
+} else whichSchool = "Lighthouse Labs"
+console.log(whichSchool)
+
+
+if (age < 13 ) {
+  console.log ("Elementary School");
+} else if (age > 13 && age < 18) {
+  console.log = ("Secondary School");
+} else  {console.log = ("Lighthouse Labs")
+
+
+
+while (x <= 20) { 
+  if (x % 3 === 0 && x % 5 === 0) {
+  console.log ("JuliaJames")
+} else if (x % 5 === 0){
+  console.log ("James")
+} else if (x % 3 === 0){
+  console.log ("Julia")
+} else {
+  console.log (x);
+  } x = x + 1
+} 
---
+++ tinkering.js
@@ -0,0 +1,2 @@
+if(1)
+    console.log("YOLO");
